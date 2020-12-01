0ae5d4584cb8181c326c38760fd34fc01f443dca
Cem Berke Cebi <cemberke.cebi@trendyol.com>
2020-12-01 15:41:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "furkankocer",
+      caption: "Best photo of the year",
+      imageUrl:
+        "https://www.freecodecamp.org/news/content/images/size/w600/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
+    },
+    {
+      username: "malionde",
+      caption: "Cool!",
+      imageUrl:
+        "https://www.freecodecamp.org/news/content/images/size/w600/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
+    },
+    {
+      username: "cemthecebi",
+      caption: "Amazing Day!",
+      imageUrl:
+        "https://www.freecodecamp.org/news/content/images/size/w600/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/*Header*/}
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          height="40px;"
+          alt=""
+        />
+      </div>
+      <h1> Cem's Instagram Clone</h1>
+
+      {/*Posts*/}
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
